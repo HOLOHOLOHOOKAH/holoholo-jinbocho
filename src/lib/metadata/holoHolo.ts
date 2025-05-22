41ab345e05f403960ee5621635d16d1045c3d92b
reidizhupa <reidizhupa56@gmail.com>
2025-05-23 02:21:26 +0900

    fix

--- src/lib/metadata/holoHolo.ts
+++ src/lib/metadata/holoHolo.ts
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 export const holoHoloMetadata: Metadata = {
 	title: "湯島ホロホロシーシャ",
 	description: "【湯島ホロホロシーシャ 上野・御徒町店】は、湯島駅徒歩1分、上野・御徒町・秋葉原からも好アクセスのシーシャバー。スタイリッシュな空間で深夜29時まで営業。東京都台東区上野1丁目。ご予約・お問い合わせは070-2644-8150まで。",
-
+	keywords: ["湯島", "上野", "御徒町", "秋葉原", "シーシャ", "東京シーシャバー", "水タバコ", "ホロホロシーシャ", "Shisha Tokyo", "Yushima Shisha"],
 	openGraph: {
 		title: "湯島ホロホロシーシャ",
 		description: "【湯島ホロホロシーシャ 上野・御徒町店】は、湯島駅徒歩1分、上野・御徒町・秋葉原からも好アクセスのシーシャバー。スタイリッシュな空間で深夜29時まで営業。東京都台東区上野1丁目。ご予約・お問い合わせは070-2644-8150まで。",
@@ -16,6 +16,12 @@ export const holoHoloMetadata: Metadata = {
 			},
 		],
 		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "湯島ホロホロシーシャ 上野・御徒町店",
+		description: "湯島駅徒歩1分。上野・秋葉原・御徒町からも好アクセスのスタイリッシュなシーシャバー。深夜29時まで営業中。ご予約は070-2644-8150まで。",
+		images: ["/yushima/android-chrome-512x512.png?v=4"],
 	},
 	robots: {
 		index: true,
--- src/lib/metadata/jinbocho.ts
+++ src/lib/metadata/jinbocho.ts
@@ -4,6 +4,8 @@ import { Metadata } from "next";
 export const jinbochoMetadata: Metadata = {
 	title: "神保町ホロホロシーシャ",
 	description: "神保町駅徒歩2分の【神保町ホロホロシーシャ】は、落ち着いた空間で極上のシーシャを楽しめる東京・千代田区の人気シーシャバー。新御茶ノ水・御茶ノ水からも徒歩圏内。13時〜24時まで年中無休で営業中。予約・お問い合わせは080-6118-4092まで。",
+	keywords: ["神保町", "シーシャ", "シーシャバー", "東京", "ホロホロシーシャ", "水タバコ", "Shisha Tokyo", "Jimbocho"],
+
 	openGraph: {
 		title: "神保町ホロホロシーシャ",
 		description: "神保町駅徒歩2分の【神保町ホロホロシーシャ】は、落ち着いた空間で極上のシーシャを楽しめる東京・千代田区の人気シーシャバー。新御茶ノ水・御茶ノ水からも徒歩圏内。13時〜24時まで年中無休で営業中。予約・お問い合わせは080-6118-4092まで。",
@@ -17,6 +19,14 @@ export const jinbochoMetadata: Metadata = {
 		],
 		type: "website",
 	},
+
+	twitter: {
+		card: "summary_large_image",
+		title: "神保町ホロホロシーシャ",
+		description: "神保町駅徒歩2分の人気シーシャバー。落ち着いた空間で極上のシーシャ体験を。新御茶ノ水・御茶ノ水からもアクセス良好。",
+		images: ["/jinbocho/android-chrome-512x512.png?v=4"],
+	},
+
 	robots: {
 		index: true,
 		follow: true,
