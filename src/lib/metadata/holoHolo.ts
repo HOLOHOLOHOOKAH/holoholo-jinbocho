cff80f3fc592102d18c79b0ed5691cd6f62710c6
reidizhupa <reidizhupa56@gmail.com>
2025-05-23 02:19:10 +0900

    fix

--- src/lib/metadata/holoHolo.ts
+++ src/lib/metadata/holoHolo.ts
@@ -1,18 +1,18 @@
 import { Metadata } from "next";
 
 export const holoHoloMetadata: Metadata = {
-	title: "ホロホロシーシャ",
-	description: "Holo Holo Shisha",
+	title: "湯島ホロホロシーシャ",
+	description: "【湯島ホロホロシーシャ 上野・御徒町店】は、湯島駅徒歩1分、上野・御徒町・秋葉原からも好アクセスのシーシャバー。スタイリッシュな空間で深夜29時まで営業。東京都台東区上野1丁目。ご予約・お問い合わせは070-2644-8150まで。",
 
 	openGraph: {
-		title: "ホロホロシーシャ",
-		description: "Holo Holo Shisha",
+		title: "湯島ホロホロシーシャ",
+		description: "【湯島ホロホロシーシャ 上野・御徒町店】は、湯島駅徒歩1分、上野・御徒町・秋葉原からも好アクセスのシーシャバー。スタイリッシュな空間で深夜29時まで営業。東京都台東区上野1丁目。ご予約・お問い合わせは070-2644-8150まで。",
 		images: [
 			{
 				url: "/okachimachi-holo/android-chrome-512x512.png?v=4",
 				width: 800,
 				height: 600,
-				alt: "Holo Holo Shisha",
+				alt: "【湯島ホロホロシーシャ 上野・御徒町店】は、湯島駅徒歩1分、上野・御徒町・秋葉原からも好アクセスのシーシャバー。スタイリッシュな空間で深夜29時まで営業。東京都台東区上野1丁目。ご予約・お問い合わせは070-2644-8150まで。",
 			},
 		],
 		type: "website",
--- src/lib/metadata/jinbocho.ts
+++ src/lib/metadata/jinbocho.ts
@@ -3,10 +3,10 @@ import { Metadata } from "next";
 
 export const jinbochoMetadata: Metadata = {
 	title: "神保町ホロホロシーシャ",
-	description: "Holo Holo Shisha Jinbocho",
+	description: "神保町駅徒歩2分の【神保町ホロホロシーシャ】は、落ち着いた空間で極上のシーシャを楽しめる東京・千代田区の人気シーシャバー。新御茶ノ水・御茶ノ水からも徒歩圏内。13時〜24時まで年中無休で営業中。予約・お問い合わせは080-6118-4092まで。",
 	openGraph: {
 		title: "神保町ホロホロシーシャ",
-		description: "Holo Holo Shisha Jinbocho",
+		description: "神保町駅徒歩2分の【神保町ホロホロシーシャ】は、落ち着いた空間で極上のシーシャを楽しめる東京・千代田区の人気シーシャバー。新御茶ノ水・御茶ノ水からも徒歩圏内。13時〜24時まで年中無休で営業中。予約・お問い合わせは080-6118-4092まで。",
 		images: [
 			{
 				url: "/jinbocho/android-chrome-512x512.png?v=4",
