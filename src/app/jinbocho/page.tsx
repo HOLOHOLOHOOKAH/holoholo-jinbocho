1fc9a2d6b4452bcde37ef3f502ff0d31195e5afe
reidizhupa <reidizhupa56@gmail.com>
2025-05-25 16:14:44 +0900

    fix

--- src/app/jinbocho/page.tsx
+++ src/app/jinbocho/page.tsx
@@ -12,18 +12,22 @@ const Homepage: React.FC = () => {
 		<span className="fade-in">
 			<Header title="神保町ホロホロシーシャ" page="homepage" pageName="jinbocho" />
 			<Hero video="/img/jinbocho/video.mp4" img="/img/jinbocho/logo.png" height={550} width={550} />
-			<div id="system" style={{ backgroundColor: "#161616" }}>
+			<div id="system">
 				{" "}
 				<CategoryPreview imageSrc="/img/jinbocho/bg12.png" title="システム" paragraph="ドリンクメニューも充実。アルコールはもちろん、豆挽きコーヒーやチャイなど豊富にご用意しております。" buttonText="もっと見る" buttonLink="/jinbocho/system" reverse={true} />
 			</div>
+			<div className="section-divider my-4"></div>
 			<div id="soft-drinks">
 				{" "}
 				<CategoryPreview imageSrc="/img/jinbocho/shishaimg.jpg" title="ソフトドリンク" paragraph="フレーバーは「FUMARI」「AZURE」「AL FAKHER」の3メーカーを主に使用しています。ニコチンフリーフレーバーのご用意もしております。" buttonText="もっと見る" buttonLink="/jinbocho/soft-drinks" />
 			</div>
-			<div id="drinks-food" style={{ backgroundColor: "#161616" }}>
+			<div className="section-divider my-4"></div>
+
+			<div id="drinks-food">
 				{" "}
 				<CategoryPreview imageSrc="/img/jinbocho/drinksimg.jpg" title="ドリンク＆フード" paragraph="ドリンクメニューも充実。アルコールはもちろん、豆挽きコーヒーやチャイなど豊富にご用意しております。" buttonText="もっと見る" buttonLink="/jinbocho/drinks-food" reverse={true} />
 			</div>
+			<div className="section-divider my-4"></div>
 
 			<div className="mb-5">
 				{" "}
