7a9af29367e9d736a72b21efda61efec790ca63c
reidizhupa <reidizhupa56@gmail.com>
2025-05-30 22:00:11 +0900

    fix logo

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -18,10 +18,18 @@ const Hero: React.FC<HeroProps> = ({ img, height, width, video }) => {
 				{isVideo ? <video src={video} autoPlay playsInline loop muted className="hero-video z-0 absolute" /> : <Image src={video} alt="Hero" className="hero-video z-0 absolute object-fit-cover" height={1080} width={1920} />}
 				{/* You can add overlay content here */}
 
-				<div className="container position-relative z-1">
-					<div className="row">
-						<div className="position-relative d-flex justify-content-center">
-							<Image loading="eager" style={{ height: height, width: width }} className="position-relative fade-in heroImg" src={img} alt="Logo" width={230} height={230} />
+				<div className="container position-relative z-1 h-100">
+					<div className="row h-100 align-items-center">
+						<div className="col-12 d-flex justify-content-center align-items-center">
+							<Image 
+								loading="eager" 
+								style={{ height: height, width: width }} 
+								className="position-relative fade-in heroImg" 
+								src={img} 
+								alt="Logo" 
+								width={230} 
+								height={230} 
+							/>
 						</div>
 					</div>
 				</div>
